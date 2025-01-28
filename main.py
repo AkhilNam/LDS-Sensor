ccee1c3e45c206acaffaa2800ea3261801a62525
Akhil <45427189+AkhilNam@users.noreply.github.com>
2025-01-28 17:56:16 -0500

    before

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyfirmata import Arduino, util
 import time
 import matplotlib.pyplot as plt
-import json
+import json #could do .txt, lets test json first 
 #import pandas as pd
 board = Arduino('COM3')
 voltage = 0.0
@@ -11,12 +11,11 @@ it.start()
 
 pin = board.get_pin('a:5:i') #a for analog, 5 for pin number, i for input 
 #define array to store values, maybe add to dataframe 
-
 volt_data = []
 duration = int(input())
 start = time.time()
 
-while time.time() - start < duration: #make while loop time based 
+while time.time() - start < duration: #make while loop time based  - done
     sensorVal = pin.read()
     
     if sensorVal is None: 
@@ -36,6 +35,7 @@ while time.time() - start < duration: #make while loop time based
 board.exit()
 fileName = input("File name?")
 x_axis = list(range(len(volt_data)))  # this will only work for 1s increments which is not reasonable
+#potential idea: record elapsed time in its own array right after volt_data.append(voltage), so it would time.time()-start
 
 with open(f"data_collected/{fileName}.json", "w") as json_file:
     json.dump({"Time": x_axis, "Voltage": volt_data}, json_file, indent = 1)
